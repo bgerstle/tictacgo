6ca5338d734ee8bda0ed4bf33cc0310c8b137160
Brian Gerstle <brian.gerstle@gmail.com>
2018-12-05 21:49:09 -0600

    Add failing tests for victory state

--- internal/app/tictacgo/game_test.go
+++ internal/app/tictacgo/game_test.go
@@ -26,19 +26,79 @@ func TestPendingGameState(t *testing.T) {
 	assert := assert.New(t)
 
 	p1 := MockPlayer{
-		PlayerInfo: PlayerInfo{Token: 'X'},
+		PlayerInfo: PlayerInfo{Token: x},
 	}
 	p2 := MockPlayer{
-		PlayerInfo: PlayerInfo{Token: 'O'},
-	}
-	g := game{
-		Player1: p1,
-		Player2: p2,
-		board:   EmptyBoard(),
+		PlayerInfo: PlayerInfo{Token: o},
 	}
 
-	state, winner := g.state()
+	pendingBoards := []Board{
+		EmptyBoard(),
+		Board{
+			spaces: []Space{
+				X, O, nil,
+				nil, nil, nil,
+				nil, nil, nil,
+			},
+		},
+	}
 
-	assert.Equal(Pending, state)
-	assert.Nil(winner)
+	for _, board := range pendingBoards {
+		g := game{
+			Player1: &p1,
+			Player2: &p2,
+			board:   board,
+		}
+
+		state, winner := g.state()
+
+		assert.Equal(Pending, state)
+		assert.Nil(winner)
+	}
+}
+
+func TestVictoryGameState(t *testing.T) {
+	assert := assert.New(t)
+
+	p1 := MockPlayer{
+		PlayerInfo: PlayerInfo{Token: x},
+	}
+	p2 := MockPlayer{
+		PlayerInfo: PlayerInfo{Token: o},
+	}
+
+	victoryTestData := []struct {
+		Board
+		Player
+	}{
+		{
+			Board{
+				spaces: []Space{
+					X, X, X,
+					nil, O, O,
+					nil, nil, nil,
+				},
+			},
+			&p1,
+		},
+	}
+
+	for _, boardAndPlayer := range victoryTestData {
+		g := game{
+			Player1: &p1,
+			Player2: &p2,
+			board:   boardAndPlayer.Board,
+		}
+
+		state, winner := g.state()
+
+		if state != Victory {
+			t.Fatalf(
+				"Expected board to be evaluated as victory, got %s. Board: \n%s",
+				state,
+				boardAndPlayer.Board.String(),
+			)
+		}
+		assert.Equal(boardAndPlayer.Player, winner)
+	}
 }
