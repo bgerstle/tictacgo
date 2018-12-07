e71c539bcafbfa1727addcd9619bc20bd55f3a28
Brian Gerstle <brian.gerstle@gmail.com>
2018-12-07 16:31:14 -0600

    Scapped msot of the stub code >.< but minmax is off to a good start
    Basic tests for the utility functions. Need more thorough tests to test
    paths through the recursive calls. Maybe should be iterative since
    golang doesn't have TCO...

--- internal/app/tictacgo/score_move.go
+++ internal/app/tictacgo/score_move.go
@@ -1,37 +1,103 @@
 package tictacgo
 
 import (
+	"fmt"
 	"math"
 	"sort"
 )
 
-type ScoringFunc func(sp Space, currentToken rune, winnerToken rune, depth int) float64
+const maxScore = math.MaxFloat64
 
-const MaxScore = math.MaxFloat64
-
-type Score struct {
-	SpaceIndex int
-	Value      float64
+type score struct {
+	spaceIndex int
+	value      float64
 }
 
-func (s Score) LessThan(otherScore Score) bool {
-	return s.Value < otherScore.Value
+func (s score) lessThan(otherScore score) bool {
+	return s.value < otherScore.value
 }
 
-type ScoreSlice []Score
+type minimax struct {
+	maxPlayer rune
+	minPlayer rune
+}
 
-func (ss *ScoreSlice) Sort() {
-	sort.Slice(*ss, func(i, j int) bool {
-		return (*ss)[i].LessThan((*ss)[j])
+// entry point to minimax algorithm.
+func (mm minimax) chooseSpot(playerToken rune, board Board) int {
+	return mm.chooseSpotAtDepth(playerToken, board, 0)
+}
+
+func (mm minimax) opponent(player rune) rune {
+	if player == mm.maxPlayer {
+		return mm.minPlayer
+	}
+	return mm.maxPlayer
+}
+
+func (mm minimax) scoreModifier(player rune) float64 {
+	if player == mm.maxPlayer {
+		return 1
+	}
+	return -1
+}
+
+func (mm minimax) minOrMax(player rune, ss []score) score {
+	sort.Slice(ss, func(i, j int) bool {
+		return ss[i].lessThan(ss[j])
 	})
+	if player == mm.maxPlayer {
+		return ss[len(ss)-1]
+	}
+	return ss[0]
 }
 
-func (b Board) ScoresFor(playerToken rune) (scores ScoreSlice) {
-	for i := range b.AvailableSpaces() {
-		scores = append(scores, Score{
-			SpaceIndex: i,
-			Value:      0.0,
-		})
+func (mm minimax) scoreVictory(winner rune, depth int) float64 {
+	return mm.scoreModifier(winner) * maxScore / float64(depth)
+}
+
+func (mm minimax) score(space int, board Board, playerToken rune, depth int) score {
+	result := board.AssignSpace(space, &playerToken)
+	state, winner := result.GameState()
+	switch state {
+	case Victory:
+		return score{
+			spaceIndex: space,
+			value:      mm.scoreVictory(*winner, depth),
+		}
+	case Tie:
+		return score{
+			spaceIndex: space,
+			value:      0,
+		}
+	}
+
+	if state != Pending {
+		panic(fmt.Sprintf("Unexpected state: %s", state))
+	}
+
+	spaces := board.AvailableSpaces()
+	if len(spaces) == 0 {
+		panic("Should have been a tie")
+	}
+
+	nextPlayer := mm.opponent(playerToken)
+	scores := mm.scoreSpaces(nextPlayer, result, depth+1)
+	return score{
+		spaceIndex: space,
+		value:      mm.minOrMax(playerToken, scores).value,
+	}
+}
+
+func (mm minimax) scoreSpaces(player rune, board Board, depth int) (scores []score) {
+	spaces := board.AvailableSpaces()
+	scores = make([]score, len(spaces))
+	for i, space := range spaces {
+		scores[i] = mm.score(space, board, player, depth)
 	}
 	return
+}
+
+func (mm minimax) chooseSpotAtDepth(playerToken rune, board Board, depth int) int {
+	scores := mm.scoreSpaces(playerToken, board, depth)
+	return mm.minOrMax(playerToken, scores).spaceIndex
 }
--- internal/app/tictacgo/score_move_test.go
+++ internal/app/tictacgo/score_move_test.go
@@ -7,15 +7,42 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestScoreMove(t *testing.T) {
-	t.Run("number of scores equal to number of available spaces", func(t *testing.T) {
+func Test_minmaxUtils(t *testing.T) {
+	mm := minimax{
+		maxPlayer: x,
+		minPlayer: o,
+	}
+
+	t.Run("opponent returns the other token", func(t *testing.T) {
 		assert := assert.New(t)
-		board := EmptyBoard()
-		assert.Len(board.ScoresFor(x), len(board.AvailableSpaces()))
+		assert.Equal(x, mm.opponent(o))
+		assert.Equal(o, mm.opponent(x))
+	})
+
+	t.Run("score modifier", func(t *testing.T) {
+		assert := assert.New(t)
+		assert.Equal(1.0, mm.scoreModifier(mm.maxPlayer))
+		assert.Equal(-1.0, mm.scoreModifier(mm.minPlayer))
+	})
+
+	t.Run("minOrMax", func(t *testing.T) {
+		assert := assert.New(t)
+		scores := []score{
+			score{
+				spaceIndex: 0,
+				value:      -1,
+			},
+			score{
+				spaceIndex: 1,
+				value:      1,
+			},
+		}
+		assert.Equal(1, mm.minOrMax(mm.maxPlayer, scores).spaceIndex)
+		assert.Equal(0, mm.minOrMax(mm.minPlayer, scores).spaceIndex)
 	})
 }
 
-func ExampleScoreMove() {
+func Example_minimax_ChooseSpot() {
 	board := Board{
 		spaces: []Space{
 			X, X, nil,
@@ -24,12 +51,17 @@ func ExampleScoreMove() {
 		},
 	}
 
-	scores := board.ScoresFor(x)
-	scores.Sort()
-	maxScore := scores[len(scores)-1]
+	mm := minimax{
+		maxPlayer: x,
+		minPlayer: o,
+	}
 
-	fmt.Printf("X's winning move in spot %d has the highest score", maxScore.SpaceIndex)
+	xsBest := mm.chooseSpot(x, board)
+	osBest := mm.chooseSpot(o, board)
 
+	fmt.Println(fmt.Sprintf("X's winning move in spot %d was chosen", xsBest))
+	fmt.Println(fmt.Sprintf("O's winning move in spot %d was chosen", osBest))
 	// Output:
-	// X's winning move in spot 2 has the highest score
+	// X's winning move in spot 2 was chosen
+	// O's winning move in spot 5 was chosen
 }
