ae07b0e548d4f06a5f5e51c2767f8d08b1d72f04
Brian Gerstle <brian.gerstle@gmail.com>
2018-12-10 16:47:02 -0600

    Ensure bots always tie eachother
    Ensuring they can't be beat each other

--- internal/app/tictacgo/bot_test.go
+++ internal/app/tictacgo/bot_test.go
@@ -15,3 +15,35 @@ func TestBotPlayer(t *testing.T) {
 
 	assert.Equal('O', bot.Info().Token)
 }
+
+func TestBotPlayer_AlwaysTies(t *testing.T) {
+	assert := assert.New(t)
+
+	botX := BotPlayer{
+		PlayerInfo{Token: 'X'},
+	}
+	botO := BotPlayer{
+		PlayerInfo{Token: 'O'},
+	}
+
+	g := Game{
+		botX,
+		botO,
+		NewBoard([2]PlayerInfo{
+			botX.Info(),
+			botO.Info(),
+		}),
+		nil,
+	}
+
+	state, winner := g.Play()
+
+	if state != Tie {
+		// bot shouldn't be beatable—even by itself!
+		t.Fatalf(
+			"Bot should not have been beatable, but result was: %s for %#v",
+			state,
+			winner)
+	}
+	assert.Nil(winner)
+}
